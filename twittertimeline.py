e99736879570854a89595d8307569c23c86f398c
ineedblood <aztec_eu@live.com>
2015-08-27 00:36:24 +0200

    obtener tweets de timeline en twitter

---
+++ twittertimeline.py
@@ -0,0 +1,6 @@
+@bot.message_handler(commands=['gettl'])
+def command_gettl(m):
+    cid = m.chat.id
+    x = tweepy.API(auth)
+    for tweets in x.user_timeline(count=2):
+        bot.send_message(cid, tweets.text)
