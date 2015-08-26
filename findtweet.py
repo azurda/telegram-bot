2737a80f72d6d78c19aff8c671208f5bb270d05e
ineedblood <aztec_eu@live.com>
2015-08-27 00:36:28 +0200

    encontrar tweets recientes

---
+++ findtweet.py
@@ -0,0 +1,7 @@
+@bot.message_handler(commands=['find'])
+def command_find(m):
+    cid = m.chat.id 
+    x = tweepy.API(auth)
+    msg = m.text[6:]
+    for tweets in x.search(q=msg, count=3, result_type='recent'):
+        bot.send_message(cid, "$" + tweets.user.screen_name + " $ " + tweets.text)
