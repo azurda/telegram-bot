6eb0c40bc299e1d21f18f16f988068e2902cfabd
ineedblood <aztec_eu@live.com>
2015-08-27 00:36:32 +0200

    busqueda en la rae

---
+++ rae.py
@@ -0,0 +1,15 @@
+@bot.message_handler(commands=['rae'])
+def command_rae(m):
+     
+      cid = m.chat.id
+      msg = m.text[5:]
+      link = urllib.urlopen("http://dulcinea.herokuapp.com/api/?query=" + msg)
+      data = json.loads(link.read())
+      for r in data['response']:
+            if 'meanings' in r:
+                bot.send_message(cid, data["response"][0]["meanings"][0]["meaning"])
+                bot.send_message(cid, data["response"][1]["meanings"][0]["meaning"])
+                break;
+            else:
+                bot.send_message(cid, "Error en la busqueda")
+                break;
