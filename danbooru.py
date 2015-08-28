eed2a09bbeee593995feed9bd05a897e7833595c
ineedblood <aztec_eu@live.com>
2015-08-28 18:26:56 +0200

    danbooru images

---
+++ danbooru.py
@@ -0,0 +1,13 @@
+@bot.message_handler(commands=['danbooru'])
+def command_danbooru(m):
+    cid = m.chat.id
+    link = urllib.urlopen("http://danbooru.donmai.us/explore/posts/popular.json")
+    
+    data = json.loads(link.read())
+    rnd_no = random.randrange(0,18)
+    img = urllib.URLopener()
+    # bot.send_message(cid, data[rnd_no]['file_url'])
+    img.retrieve("http://danbooru.donmai.us/"+ data[rnd_no]['file_url'], "danbooru_tmp.jpg")
+    bot.send_photo(cid, open('danbooru_tmp.jpg', 'rb'))
+    # tag_string_artist tag_string_character
+    bot.send_message(cid, "Artist: " + data[rnd_no]['tag_string_artist'] + "\n Character: " + data[rnd_no]['tag_string_character'])
