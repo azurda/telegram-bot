0e30a1bf88943fc0553c5590ccdc4a62a4123cb3
ineedblood <aztec_eu@live.com>
2015-08-28 17:31:04 +0200

    google imagesearch feature

---
+++ google_imgsearch.py
@@ -0,0 +1,10 @@
+@bot.message_handler(commands=['img'])
+def command_img(m):
+    cid = m.chat.id
+    token = m.text[5:]
+    link = urllib.urlopen("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + token)
+    data = json.loads(link.read())
+    rnd_no = random.randrange(0,4)
+    image = urllib.URLopener()
+    image.retrieve(data['responseData']['results'][rnd_no]['unescapedUrl'], "tmp.jpg")
+    bot.send_photo(cid, open( 'tmp.jpg', 'rb'))
