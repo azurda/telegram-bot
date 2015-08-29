eaa4d86d00a04a1a060545c42aa8c9aea74c196d
ineedblood <aztec_eu@live.com>
2015-08-29 20:59:17 +0200

    obtain moar pics / not only popular

--- danbooru.py
+++ danbooru.py
@@ -1,13 +1,22 @@
 @bot.message_handler(commands=['danbooru'])
 def command_danbooru(m):
     cid = m.chat.id
-    link = urllib.urlopen("http://danbooru.donmai.us/explore/posts/popular.json")
+    rnd_no_c = random.randrange(0,1)
+    if(rnd_no_c == 0):
+        link = urllib.urlopen("http://danbooru.donmai.us/explore/posts/popular.json")
     
-    data = json.loads(link.read())
-    rnd_no = random.randrange(0,18)
-    img = urllib.URLopener()
-    # bot.send_message(cid, data[rnd_no]['file_url'])
-    img.retrieve("http://danbooru.donmai.us/"+ data[rnd_no]['file_url'], "danbooru_tmp.jpg")
-    bot.send_photo(cid, open('danbooru_tmp.jpg', 'rb'))
+        data = json.loads(link.read())
+        rnd_no = random.randrange(0,18)
+        img = urllib.URLopener()
+        # bot.send_message(cid, data[rnd_no]['file_url'])
+        img.retrieve("http://danbooru.donmai.us/"+ data[rnd_no]['file_url'], "danbooru_tmp.jpg")
+        bot.send_photo(cid, open('danbooru_tmp.jpg', 'rb'))
     # tag_string_artist tag_string_character
-    bot.send_message(cid, "Artist: " + data[rnd_no]['tag_string_artist'] + "\n Character: " + data[rnd_no]['tag_string_character'])
+        bot.send_message(cid, "Artist: " + data[rnd_no]['tag_string_artist'] + "\n Character: " + data[rnd_no]['tag_string_character'])
+    else:
+        link = urllib.urlopen("http://danbooru.donmai.us/posts.json")
+        data = json.loads(link.read())
+        rnd_no = random.randrange(0,18)
+        img.retrieve("http://danbooru.donmai.us/"+ data[rnd_no]['file_url'], "danbooru_tmp.jpg")
+        bot.send_photo(cid, open('danbooru_tmp.jpg', 'rb'))
+        bot.send_message(cid, "Artist: " + data[rnd_no]['tag_string_artist'] + "\n Character: " + data[rnd_no]['tag_string_character'])
