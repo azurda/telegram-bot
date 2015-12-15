3d67cf26e86e8cb003061c37fb54681f422174af
ineedblood <aztec_eu@live.com>
2015-12-15 11:18:46 +0100

    codigo básico para comenzar el bot
    con este código, se pueden añadir los módulos para que funcionen sin problemas

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import telebot # Librería de la API del bot.
+from telebot import types # Tipos para la API del bot.
+import time # Librería para hacer que el programa que controla el bot no se acabe.
+
+TOKEN = '131446566:AAHGutFdsxgf9X5YDKzxC__LkTAFrjSnFiQ' # Nuestro tokken del bot (el que @BotFather nos dió).
+
+bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
+
+def listener(messages): # Con esto, estamos definiendo una función llamada 'listener', que recibe como parámetro un dato llamado 'messages'.
+    for m in messages: # Por cada dato 'm' en el dato 'messages'
+        cid = m.chat.id # Almacenaremos el ID de la conversación.
+        print "[" + str(cid) + "]: " + m.text # Y haremos que imprima algo parecido a esto -> [52033876]: /start
+
+bot.set_update_listener(listener) # Así, le decimos al bot que utilice como función escuchadora nuestra función 'listener' declarada arriba.
+
+bot.polling(none_stop=True) # Con esto, le decimos al bot que siga funcionando incluso si encuentra algún fallo.
